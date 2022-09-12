b2d72d235e5cad20754b07920512dfbad8c5a056
Mootbing <kingofcowlandmrmoooo@gmail.com>
2022-09-12 15:35:05 -0400

    analysis page start

--- src/App.js
+++ src/App.js
@@ -5,29 +5,34 @@ import { useState } from "react";
 import Blocked from "./components/Blocked";
 import Divider from "./components/Divider";
 import Time from "./pages/Time";
+import Analytics from "./pages/Analytics";
 
 function App() {
+
+  const [page, setPage] = useState("Time");
+
   return (
-    <div className="top-no-bar" style={{ width: "350px", padding: "25px", paddingTop: 0, backgroundColor: "#232323" }}>
+    <div className="top-no-bar" style={{ width: page === "Time" ? "350px" : "500px", padding: "25px", paddingTop: 0, backgroundColor: "#232323" }}>
       <div style={{ width: "300px", color: "white"}}>
         <div style={{display: "flex"}}>
           <button
             style={{
               width: "100%",
-              height: 40,
+              height: page === "Time" ? 40 : 25,
               backgroundColor: "#303030",
               color: "#999",
               border: 0,
               borderBottomRightRadius: 15,
               borderBottomLeftRadius: 15,
             }}
+            onClick = {() => {setPage("Time")}}
           >
             Time
           </button>
           <button
             style={{
               width: "100%",
-              height: 25,
+              height: page === "Analytics" ? 40 : 25,
               backgroundColor: "#282828",
               color: "#999",
               border: 0,
@@ -35,12 +40,15 @@ function App() {
               borderBottomRightRadius: 15,
               borderBottomLeftRadius: 15,
             }}
+            onClick = {() => {setPage("Analytics")}}
           >
             Analysis
           </button>
         </div>
 
-        <Time />
+        {page === "Time" && <Time />}
+        {page === "Analytics" && <Analytics />}
+      
       </div>
     </div>
   );
