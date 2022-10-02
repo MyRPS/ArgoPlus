705dd7efd9e1ae8dbfe1b00a09cd4ec8f2088853
Mootbing <50122069+Mootbing@users.noreply.github.com>
2022-10-02 10:38:52 -0400

    grade sim

--- public/scripts/content/assignDetails.js
+++ public/scripts/content/assignDetails.js
@@ -2,6 +2,123 @@
 
 console.log("Argo+: html script setup")
 
+const injectFinalGrade = (resultsDiv, finalGrade, className) => {
+
+    while (resultsDiv.firstChild)
+    {
+        resultsDiv.removeChild(resultsDiv.firstChild);
+    }
+
+    const resultThisAss = document.createElement("p");
+    resultThisAss.innerHTML = `${finalGrade}%`;
+    resultThisAss.style.fontSize = "30px";
+    resultThisAss.style.fontWeight = "bold";
+
+    const resultThisAssSubtext = document.createElement("p");
+    resultThisAssSubtext.innerHTML = "On this assignment";
+
+    const resultOverall = document.createElement("p");
+    resultOverall.innerHTML = `WIP %`;
+    resultOverall.style.fontSize = "30px";
+    resultOverall.style.fontWeight = "bold";
+
+    const resultOverallSubtext = document.createElement("p");
+    resultOverallSubtext.innerHTML = `In ${className}`;
+    
+    resultsDiv.appendChild(resultThisAss);
+    resultsDiv.appendChild(resultThisAssSubtext);
+    resultsDiv.appendChild(resultOverall);
+    resultsDiv.appendChild(resultOverallSubtext);
+}
+
+const injectGradeSimulator = (assignmentDetail) => {
+    
+    const injectionLocation = document.getElementsByClassName("bb-tile-content-section");
+    let earnedGrade = document.getElementsByClassName("assignment-detail-status-label")[0];
+
+    if (injectionLocation.length === 0 || earnedGrade === undefined || injectionLocation[0] === undefined)
+    {
+        setTimeout(() => {injectGradeSimulator(assignmentDetail)}, 500);
+        return;
+    }
+
+    earnedGrade = earnedGrade.innerHTML;
+
+    if (earnedGrade.includes("Graded:") || assignmentDetail["MaxPoints"] === 0)
+    {
+        return;
+    }
+
+    const gradeSimulator = document.createElement("div");
+    gradeSimulator.style.width = "100%";
+    gradeSimulator.style.height = "100%";
+    gradeSimulator.style.backgroundColor = "#fff";
+    gradeSimulator.style.color = "#000";
+    gradeSimulator.style.textAlign = "center";
+    gradeSimulator.style.paddingTop = "6px";
+    gradeSimulator.style.marginRight = "25px";
+    gradeSimulator.style.marginTop = "5px";
+    gradeSimulator.style.position = "relative";
+    gradeSimulator.className = "ArgoPlus-Display";
+
+    const gradeSimulatorText = document.createElement("p");
+    gradeSimulatorText.innerHTML = "Grade Simulator (SUPER BETA)";
+    gradeSimulatorText.style.display = "inline-block";
+    gradeSimulatorText.style.marginLeft = "10px";
+    gradeSimulatorText.style.marginRight = "10px";
+    gradeSimulatorText.style.fontSize = "30px";
+    gradeSimulatorText.style.fontWeight = "bold";
+
+    console.log(earnedGrade);
+
+    const resultsDiv = document.createElement("div");
+
+    const gradeSimulatorInput = document.createElement("input");
+    gradeSimulatorInput.type = "number";
+    gradeSimulatorInput.max = assignmentDetail["MaxPoints"];
+    gradeSimulatorInput.min = "0";
+    gradeSimulatorInput.value = assignmentDetail["MaxPoints"];
+    gradeSimulatorInput.style.width = "100px";
+    gradeSimulatorInput.style.height = 50;
+    gradeSimulatorInput.style.fontSize = "12px";
+    gradeSimulatorInput.style.padding = "5px";
+    gradeSimulatorInput.style.marginBottom = "5px";
+    gradeSimulatorInput.onchange = () => {
+        const grade = gradeSimulatorInput.value;
+        const maxGrade = assignmentDetail["MaxPoints"];
+        const gradePercentage = (grade / maxGrade) * 100;
+        injectFinalGrade(resultsDiv, gradePercentage, assignmentDetail["SectionLinks"][0]["Section"]["Name"]);
+    }
+
+    const gradeSimulatorInputSubtext = document.createElement("p");
+    gradeSimulatorInputSubtext.innerHTML = "/ " + assignmentDetail["MaxPoints"];
+    gradeSimulatorInputSubtext.width = "50px";
+    gradeSimulatorInputSubtext.style.display = "inline-block";
+    gradeSimulatorInputSubtext.style.marginInlineStart = "5px";
+    gradeSimulatorInputSubtext.style.fontSize = "20px";
+
+    // const gradeSimulatorButton = document.createElement("button");
+    // gradeSimulatorButton.innerHTML = "Calculate";
+    // gradeSimulatorButton.style.width = "300px";
+    // gradeSimulatorButton.style.padding = "5px";
+    // gradeSimulatorButton.style.border = "none";
+    // gradeSimulatorButton.style.backgroundColor = "#707070";
+    // gradeSimulatorButton.style.color = "#fff";
+    // gradeSimulatorButton.style.borderRadius = "25px";
+    // gradeSimulatorButton.style.height = 50;
+    // gradeSimulatorButton.onclick
+
+    gradeSimulator.appendChild(gradeSimulatorText);
+    gradeSimulator.appendChild(gradeSimulatorInput);
+    gradeSimulator.appendChild(gradeSimulatorInputSubtext);
+    // gradeSimulator.appendChild(gradeSimulatorButton);
+    gradeSimulator.appendChild(resultsDiv);
+
+    injectionLocation[1].appendChild(gradeSimulator);
+
+    injectFinalGrade(resultsDiv, 100, assignmentDetail["SectionLinks"][0]["Section"]["Name"]);
+}
+
 const injectDisplay = (label, color, isHeader = false, showBeta = true, URL = "", fontColor = "#fff") => {
 
     const injectionLocation = document.getElementsByClassName("bb-tile-content-section");
@@ -103,6 +220,8 @@ const checkForAssDetailUrl = async (request) => {
 
         injectDisplay("Tags (Beta): ", "#fff", true, false, "", "#272727");
 
+        injectDisplay("Assignment Type: " + assignmentDetail["AssignmentType"] , "#2DC8D0");
+
         if (assignmentDetail["DropboxResub"])
         {
             injectDisplay("Resubmittable Until Deadline", "#2DC8D0");
@@ -119,7 +238,7 @@ const checkForAssDetailUrl = async (request) => {
 
             for (var downloadKey of assignmentDetail["DownloadItems"])
             {
-                injectDisplay(downloadKey["ShortDescription"], "#EEE", false, false, downloadKey["DownloadUrl"]);
+                injectDisplay(downloadKey["ShortDescription"] + " (" + downloadKey["FriendlyFileName"] + ")", "#EEE", false, false, downloadKey["DownloadUrl"]);
             }
         }
 
@@ -132,6 +251,8 @@ const checkForAssDetailUrl = async (request) => {
                 injectDisplay(linkKey["ShortDescription"], "#EEE", false, false, linkKey["Url"]);
             }
         }
+
+        injectGradeSimulator(assignmentDetail);
     }
 }
 
